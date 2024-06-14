2894103f8380bae8a79df8e09d758e6d71e62779
Rahul suthar <sutharrahul119@gmail.com>
2024-06-14 12:41:19 +0530

    FeaturedCourses section created

---
+++ src/components/Button.tsx
@@ -0,0 +1,80 @@
+"use client";
+import React from "react";
+import reactElementToJSXString from "react-element-to-jsx-string";
+import { toast, Toaster } from "sonner";
+import { ButtonsCard } from "./ui/tailwindcss-buttons";
+
+export function TailwindcssButtons() {
+  const copy = (button: any) => {
+    if (button.code) {
+      copyToClipboard(button.code);
+      return;
+    }
+    let buttonString = reactElementToJSXString(button.component);
+
+    if (buttonString) {
+      const textToCopy = buttonString;
+      copyToClipboard(textToCopy);
+    }
+  };
+
+  const copyToClipboard = (text: string) => {
+    navigator.clipboard
+      .writeText(text)
+      .then(() => {
+        console.log("Text copied to clipboard:", text);
+        toast.success("Copied to clipboard");
+      })
+      .catch((err) => {
+        console.error("Error copying text to clipboard:", err);
+        toast.error("Error copying to clipboard");
+      });
+  };
+  return (
+    <div className="pb-40 px-4 w-full">
+      <Toaster position="top-center" />
+      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 w-full  max-w-7xl mx-auto gap-10">
+        {buttons.map((button, idx) => (
+          <ButtonsCard key={idx} onClick={() => copy(button)}>
+            {button.component}
+          </ButtonsCard>
+        ))}
+      </div>
+    </div>
+  );
+}
+export const buttons = [
+  {
+    name: "Shimmer",
+    description: "Shimmer button for your website",
+    showDot: false,
+    component: (
+      <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+        Shimmer
+      </button>
+    ),
+    code: `
+        // Button code
+        <button className="inline-flex h-12 animate-shimmer items-center justify-center rounded-md border border-slate-800 bg-[linear-gradient(110deg,#000103,45%,#1e2631,55%,#000103)] bg-[length:200%_100%] px-6 font-medium text-slate-400 transition-colors focus:outline-none focus:ring-2 focus:ring-slate-400 focus:ring-offset-2 focus:ring-offset-slate-50">
+          Shimmer
+        </button>
+  
+        // tailwind.config.js code
+        {
+          "animation": {
+            shimmer: "shimmer 2s linear infinite"
+          },
+          "keyframes": {
+            shimmer: {
+              from: {
+                "backgroundPosition": "0 0"
+              },
+              to: {
+                "backgroundPosition": "-200% 0"
+              }
+            }
+          }
+        }
+      `,
+  },
+];
